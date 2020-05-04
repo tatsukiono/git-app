0c73e8ef92187008f6a440e7cdf00b50e3472e42
tatsukiono <64731027+tatsukiono@users.noreply.github.com>
2020-05-04 12:45:51 +0900

    Update articles_controller.rb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,23 +1,21 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
 
-  # GET /articles
-  # GET /articles.json
+
   def index
     @articles = Article.all
   end
 
-  # GET /articles/1
-  # GET /articles/1.json
+
   def show
   end
 
-  # GET /articles/new
+
   def new
     @article = Article.new
   end
 
-  # GET /articles/1/edit
+
   def edit
   end
 
